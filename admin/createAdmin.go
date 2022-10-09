5b729fcda7af2d0fc1aa2ff49268872f812f523c
Ethan Damien <ethanchang.d@gmail.com>
2022-10-09 17:30:00 -0400

    refactoring the project

--- admin/createAdmin.go
+++ admin/createAdmin.go
@@ -12,6 +12,7 @@ type CreateAdminRequest struct {
 }
 
 type CreateAdminResponse struct {
+	DESC  string `json:"desc"`
 	OK    bool   `json:"ok"`
 	ID    int64  `json:"id"`
 	ReqID string `json:"req_id"`
@@ -19,11 +20,11 @@ type CreateAdminResponse struct {
 
 func CreateAdmin(_ context.Context, reqID string, req CreateAdminRequest) (CreateAdminResponse, error) {
 	if req.FirstName == "" {
-		return CreateAdminResponse{OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("the first_name is missing")
+		return CreateAdminResponse{DESC: "CreateAdmin err", OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("the first_name is missing")
 	}
 	if req.LastName == "" {
-		return CreateAdminResponse{OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("the last_name is missing")
+		return CreateAdminResponse{DESC: "CreateAdmin err", OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("the last_name is missing")
 	}
 
-	return CreateAdminResponse{OK: true, ID: time.Now().UnixNano(), ReqID: reqID}, nil
+	return CreateAdminResponse{DESC: "CreateAdmin success", OK: true, ID: time.Now().UnixNano(), ReqID: reqID}, nil
 }
--- user/createUser.go
+++ user/createUser.go
@@ -12,6 +12,7 @@ type CreateUserRequest struct {
 }
 
 type CreateUserResponse struct {
+	DESC  string `json:"desc"`
 	OK    bool   `json:"ok"`
 	ID    int64  `json:"id"`
 	ReqID string `json:"req_id"`
@@ -19,11 +20,11 @@ type CreateUserResponse struct {
 
 func CreateUser(_ context.Context, reqID string, req CreateUserRequest) (CreateUserResponse, error) {
 	if req.FirstName == "" {
-		return CreateUserResponse{OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("the first_name is missing")
+		return CreateUserResponse{DESC: "CreateUser err", OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("the first_name is missing")
 	}
 	if req.LastName == "" {
-		return CreateUserResponse{OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("the last_name is missing")
+		return CreateUserResponse{DESC: "CreateUser err", OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("the last_name is missing")
 	}
 
-	return CreateUserResponse{OK: true, ID: time.Now().UnixNano(), ReqID: reqID}, nil
+	return CreateUserResponse{DESC: "CreateUser success", OK: true, ID: time.Now().UnixNano(), ReqID: reqID}, nil
 }
