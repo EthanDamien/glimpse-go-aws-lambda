0c484e42917d7a14bfcd52948477dc6e4226541e
KARL SEQUEIRA <karlsequeira@nbp-28-234.nbp.ruw.rutgers.edu>
2022-10-28 12:14:58 -0400

    login fixed and returns info

--- login/login.go
+++ login/login.go
@@ -4,7 +4,25 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
 )
+
+type Admin struct {
+	AdminID      string `json:"AdminID"`
+	Email        string `json:"Email"`
+	Company_Name string `json:"Company_Name"`
+	AdminPIN     string `json:"AdminPIN"`
+}
+
+type Employee struct {
+	EmployeeID string `json:"employeeID"`
+	AdminID    string `json:"AdminID"`
+	Email      string `json:"email"`
+	FirstName  string `json:"firstName"`
+	LastName   string `json:"lastName"`
+	Birthday   string `json:"birthday"`
+	JobTitle   string `json:"jobTitle"`
+}
 
 type EmployeeLoginRequest struct {
 	Email    string `json:"email"`
@@ -16,42 +34,102 @@ type AdminLoginRequest struct {
 	Password string `json:"password"`
 }
 
-type LoginResponse struct {
-	DESC  string `json:"desc"`
-	OK    bool   `json:"ok"`
-	ID    int64  `json:"id"`
-	ReqID string `json:"req_id"`
+type EmployeeLoginResponse struct {
+	RES   []Employee `json:"res"`
+	DESC  string     `json:"desc"`
+	OK    bool       `json:"ok"`
+	ID    int64      `json:"id"`
+	ReqID string     `json:"req_id"`
 }
 
-const adminLogin = `
-SELECT JSON_ARRAYAGG(JSON_OBJECT('AdminID', AdminID, 'Email', Email, 'Password', 
-Password, 'Company_Name', Company_Name, 'AdminPIN', AdminPIN)) from Admins WHERE Email = "%s" AND Password = "%s";
-`
-const employeeLogin = `
-SELECT JSON_ARRAYAGG(JSON_OBJECT('EmployeeID', EmployeeID, 'AdminID', AdminID, 'Email', Email, 'Password', 
-Password, 'FirstName', FirstName, 'LastName', LastName, 'Birthday', Birthday, 'JobTitle', JobTitle)) from Employees WHERE Email = "%s" AND Password = "%s";
-`
+type AdminLoginResponse struct {
+	RES   []Admin `json:"res"`
+	DESC  string  `json:"desc"`
+	OK    bool    `json:"ok"`
+	ID    int64   `json:"id"`
+	ReqID string  `json:"req_id"`
+}
 
-func AdminLogin(ctx context.Context, reqID string, req AdminLoginRequest, db *sql.DB) (LoginResponse, error) {
+const employeeLogin = `SELECT EmployeeID, AdminID, Email, FirstName, LastName, Birthday, JobTitle FROM Employees WHERE Email = "%s" AND Password = "%s";`
+
+const adminLogin = `SELECT AdminID, Email, Company_Name, AdminPIN FROM Admins WHERE Email = "%s" AND Password = "%s";`
+
+func AdminLogin(ctx context.Context, reqID string, req AdminLoginRequest, db *sql.DB) (AdminLoginResponse, error) {
+	if req.Email == "" {
+		return AdminLoginResponse{DESC: "Could not get admin - missing Email", OK: false, ID: time.Now().UnixNano(), ReqID: reqID}, fmt.Errorf("Missing Email")
+	}
+	if req.Password == "" {
+		return AdminLoginResponse{DESC: "Could not get admin - missing Password", OK: false, ID: time.Now().UnixNano(), ReqID: reqID}, fmt.Errorf("Missing Password")
+	}
+
 	var query = fmt.Sprintf(adminLogin, req.Email, req.Password)
-	rows, err := db.QueryContext(ctx, query)
+	res, err := getQueryResAdmin(query, db)
+	if res == nil {
+		return AdminLoginResponse{DESC: "Could not get admin - incorrect email/password", OK: false, ID: time.Now().UnixNano(), ReqID: reqID}, fmt.Errorf("Incorrect Email and/or Password")
+	}
 	if err != nil {
-		return LoginResponse{DESC: "Error Querying Admins Table", OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("could not login as admin")
+		return AdminLoginResponse{DESC: "Could not get admin", OK: false, ID: time.Now().UnixNano(), ReqID: reqID}, err
 	}
-	if rows == nil {
-		return LoginResponse{DESC: "Error Querying Admins Table", OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("Invalid email or password/admin does not exist")
-	}
-	return LoginResponse{DESC: "Successful Admin Login"}, nil
+	return AdminLoginResponse{RES: res, OK: true, ID: time.Now().UnixNano(), ReqID: reqID}, nil
+
 }
 
-func EmployeeLogin(ctx context.Context, reqID string, req EmployeeLoginRequest, db *sql.DB) (LoginResponse, error) {
+func EmployeeLogin(ctx context.Context, reqID string, req EmployeeLoginRequest, db *sql.DB) (EmployeeLoginResponse, error) {
+	if req.Email == "" {
+		return EmployeeLoginResponse{DESC: "Could not get employee - missing Email", OK: false, ID: time.Now().UnixNano(), ReqID: reqID}, fmt.Errorf("Missing Email")
+	}
+	if req.Password == "" {
+		return EmployeeLoginResponse{DESC: "Could not get employee - missing Password", OK: false, ID: time.Now().UnixNano(), ReqID: reqID}, fmt.Errorf("Missing Password")
+	}
+
 	var query = fmt.Sprintf(employeeLogin, req.Email, req.Password)
-	rows, err := db.QueryContext(ctx, query)
+	res, err := getQueryResEmployee(query, db)
+	if res == nil {
+		return EmployeeLoginResponse{DESC: "Could not get employee - incorrect email/password", OK: false, ID: time.Now().UnixNano(), ReqID: reqID}, fmt.Errorf("Incorrect Email and/or Password")
+	}
 	if err != nil {
-		return LoginResponse{DESC: "Error Querying Employees Table", OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("Could not login as employee")
+		return EmployeeLoginResponse{DESC: "Could not get admin", OK: false, ID: time.Now().UnixNano(), ReqID: reqID}, err
 	}
-	if rows == nil {
-		return LoginResponse{DESC: "Error Querying Employees Table", OK: false, ID: 0, ReqID: reqID}, fmt.Errorf("Invalid email or password/employee does not exist")
+	return EmployeeLoginResponse{RES: res, OK: true, ID: time.Now().UnixNano(), ReqID: reqID}, nil
+
+}
+
+func getQueryResAdmin(builtQuery string, db *sql.DB) ([]Admin, error) {
+	rows, err := db.Query(builtQuery)
+
+	if err != nil {
+		return nil, err
 	}
-	return LoginResponse{DESC: "Successful Employee Login"}, nil
+	defer rows.Close()
+
+	var adminInfo []Admin
+
+	for rows.Next() {
+		var admin Admin
+		if err := rows.Scan(&admin.AdminID, &admin.Email, &admin.Company_Name, &admin.AdminPIN); err != nil {
+			return adminInfo, err
+		}
+		adminInfo = append(adminInfo, admin)
+	}
+	return adminInfo, nil
+}
+
+func getQueryResEmployee(builtQuery string, db *sql.DB) ([]Employee, error) {
+	rows, err := db.Query(builtQuery)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var employeeInfo []Employee
+
+	for rows.Next() {
+		var employee Employee
+		if err := rows.Scan(&employee.EmployeeID, &employee.AdminID, &employee.Email, &employee.FirstName, &employee.LastName, &employee.Birthday, &employee.JobTitle); err != nil {
+			return employeeInfo, err
+		}
+		employeeInfo = append(employeeInfo, employee)
+	}
+	return employeeInfo, nil
 }
